953c85ae22b1a311f407e5fb358efb7241d46e14
AtesiT <atesit@mail.ru>
2025-11-11 16:04:59 +0300

    Initial Commit

---
+++ TrackListOfMusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackListOfMusicApp
+//
+//  Created by Yarutov on 11.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
